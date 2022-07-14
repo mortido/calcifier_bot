244aebcfa5984b0076b9075dbca6f418c6c43c7a
mortido <open.for.good@gmail.com>
2022-07-14 18:14:34 +0300

    up

--- src/calcifier.py
+++ src/calcifier.py
@@ -41,7 +41,7 @@ def main():
     logger.info(f"Starting Calcifier bot... ")
     logger.info(f"Persistent file: {config.persistent_file}")
     logger.info(f"Bot admins: {', '.join(config.bot_admins)}")
-    logger.info(f"Horse chat: {', '.join(config.horse_chat)}")
+    logger.info(f"Horse chat: {config.horse_chat}")
 
     # Using JSON can broke some sets
     persistent_storage = PicklePersistence(filepath=config.persistent_file)
